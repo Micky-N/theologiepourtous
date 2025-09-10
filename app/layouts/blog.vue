2b1be43869db66f50a38dc12bfb10760e3a1534a
Mickaël Ndinga <84698581+Micky-N@users.noreply.github.com>
2025-09-10 22:24:20 +0200

    Add new articles on dangerous animals, cryptocurrencies, Pyrenees exploration, James Webb Space Telescope, meditation benefits, and Asian cuisine
    - Created a comprehensive article detailing the 10 most dangerous creatures on Earth, highlighting their characteristics and threats to humans.
    - Introduced an article discussing the rise of cryptocurrencies, their evolution, opportunities, and challenges in the financial landscape.
    - Added a piece exploring the majestic peaks of the Pyrenees, focusing on its natural beauty, cultural heritage, and outdoor adventures.
    - Developed an article on the James Webb Space Telescope, detailing its construction, launch, and legacy of discovery in astronomy.
    - Included an article on the benefits of meditation and mindfulness practices for mental health, emphasizing their positive effects on well-being.
    - Compiled a detailed exploration of Asian cuisine, featuring recipes and insights into the diverse culinary traditions across the continent.

--- app/layouts/blog.vue
+++ app/layouts/blog.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
-const { data: page } = await useAsyncData('blog', () => queryCollection('blog').first())
-const { data: posts } = await useAsyncData('themes', () => queryCollection('posts').select('id', 'theme').all())
+const { data: page } = useAsyncData('blog', () => queryCollection('blog').first())
+const { data: posts } = useAsyncData('themes', () => queryCollection('posts').select('id', 'theme').all())
 
 const countPostsByTheme = computed(() => {
     const themes: Record<string, number> = {}
@@ -23,37 +23,41 @@ const countPostsByTheme = computed(() => {
             to="/blog"
             mode="slideover"
         >
-            <UNavigationMenu
-                v-if="page"
-                :items="page.sections"
-                :ui="{
-                    list: 'gap-10',
-                    linkTrailingIcon: 'hidden'
-                }"
-            >
-                <template #item-content="{ item }">
-                    <UBlogPost
-                        :title="item.label"
-                        :description="item.description"
-                        :image="item.image"
-                        :to="item.to"
-                        orientation="horizontal"
-                        variant="ghost"
-                        :badge="countPostsByTheme[item.slug] + ' articles'"
-                    />
-                </template>
-            </UNavigationMenu>
-            <template #body>
+            <ClientOnly>
                 <UNavigationMenu
                     v-if="page"
-                    variant="link"
-                    :highlight="true"
                     :items="page.sections"
-                    orientation="vertical"
                     :ui="{
-                        list: 'space-y-2'
+                        list: 'gap-10',
+                        linkTrailingIcon: 'hidden'
                     }"
-                />
+                >
+                    <template #item-content="{ item }">
+                        <UBlogPost
+                            :title="item.label"
+                            :description="item.description"
+                            :image="item.image"
+                            :to="item.to"
+                            orientation="horizontal"
+                            variant="ghost"
+                            :badge="countPostsByTheme[item.slug] + ' articles'"
+                        />
+                    </template>
+                </UNavigationMenu>
+            </ClientOnly>
+            <template #body>
+                <ClientOnly>
+                    <UNavigationMenu
+                        v-if="page"
+                        variant="link"
+                        :highlight="true"
+                        :items="page.sections"
+                        orientation="vertical"
+                        :ui="{
+                            list: 'space-y-2'
+                        }"
+                    />
+                </ClientOnly>
             </template>
         </UHeader>
 
