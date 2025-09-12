878c60a802e2d5eb6d71e332afccf82e084ead3c
Mickaël Ndinga <84698581+Micky-N@users.noreply.github.com>
2025-09-12 02:02:45 +0200

    style: ajuster la hauteur maximale de l'image d'en-tête du blog pour une meilleure présentation

--- app/pages/blog/[theme]/index.vue
+++ app/pages/blog/[theme]/index.vue
@@ -32,7 +32,7 @@ useHead({
     <UPage v-if="theme">
         <NuxtImg
             :src="theme?.image.src || '/images/hero-blog.webp'"
-            class="w-full aspect-video max-h-[44rem] object-cover object-center"
+            class="w-full aspect-video max-h-[32rem] object-cover object-center"
         />
         <UContainer>
             <UPageHeader
