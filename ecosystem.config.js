111fa90d7dad6b735182922067695c56ced4152b
Mickaël Ndinga <84698581+Micky-N@users.noreply.github.com>
2025-10-04 21:30:59 +0200

    feat: ajouter la configuration de déploiement et les fichiers d'environnement pour le développement

---
+++ ecosystem.config.js
@@ -0,0 +1,44 @@
+const args = process.argv;
+
+/**
+ * Parse command line arguments into an options object
+ * @type {Object.<string, string>}
+ */
+const options = {};
+
+args.forEach((arg) => {
+    const [key, value] = arg.split('=');
+    if (key && value) {
+        options[key] = value;
+    }
+});
+
+/**
+ * Base configuration for deployment
+ *
+ * @param {'development' | 'production'} mode
+ *
+ * @return {{repo: string, fetch: string, "post-deploy": string}}
+ *
+ */
+function baseConfig(mode) {
+    return {
+        'repo': 'git@github.com:Micky-N/theologiepourtous.git',
+        'fetch': '--all',
+        'post-deploy': `cp .env.${mode} .env && npm install && npm run build`
+    };
+}
+
+module.exports = {
+    // Deployment Configuration
+    deploy: {
+        development: {
+            ...baseConfig('development'),
+            user: 'micky',
+            host: '212.132.112.230',
+            keep_releases: 1,
+            ref: 'origin/' + (options.branch || 'develop'),
+            path: '/home/micky/theologiepourtous'
+        }
+    }
+};
