cc931ed3c1afcba7632ab3ccee570fb35c68bda4
Micky-N <ndingamickael@gmail.com>
2026-03-24 00:11:58 +0100

    refactor: simplifier le rendu des composants dans la page des enseignements

--- app/pages/enseignements/[theme]/[slug].vue
+++ app/pages/enseignements/[theme]/[slug].vue
@@ -108,37 +108,17 @@ const progressButtonProps = computed<{
 
 <template>
     <UContainer v-if="lesson">
-        <img
-            :src="lesson.image.src"
-            class="w-full aspect-video rounded-b-xl max-h-[32rem] object-cover object-center"
-        >
-        <UPageHeader
-            :title="lesson.title"
-            :description="lesson.description"
-        >
+        <img :src="lesson.image.src" class="w-full aspect-video rounded-b-xl max-h-[32rem] object-cover object-center">
+        <UPageHeader :title="lesson.title" :description="lesson.description">
             <template #headline>
                 <div class="flex flex-col lg:flex-row items-start lg:items-center space-x-1">
                     <template v-if="loggedIn">
-                        <UButton
-                            v-if="status === 'success'"
-                            :icon="progressButtonProps.icon"
-                            :color="progressButtonProps.color"
-                            :label="progressButtonProps.label"
-                            :variant="progressButtonProps.variant"
-                            size="xs"
-                            @click="setProgress(lesson.slug)"
-                        />
-                        <UButton
-                            v-else-if="['idle', 'pending'].includes(status)"
-                            color="secondary"
-                            variant="subtle"
-                            size="xs"
-                            disabled
-                        >
-                            <UIcon
-                                name="i-lucide-loader-2"
-                                class="animate-spin"
-                            />
+                        <UButton v-if="status === 'success'" :icon="progressButtonProps.icon"
+                            :color="progressButtonProps.color" :label="progressButtonProps.label"
+                            :variant="progressButtonProps.variant" size="xs" @click="setProgress(lesson.slug)" />
+                        <UButton v-else-if="['idle', 'pending'].includes(status)" color="secondary" variant="subtle"
+                            size="xs" disabled>
+                            <UIcon name="i-lucide-loader-2" class="animate-spin" />
                         </UButton>
                     </template>
                     <time class="text-muted">{{ new Date(lesson.date).toLocaleDateString('fr', {
@@ -146,15 +126,12 @@ const progressButtonProps = computed<{
                         month:
                             'long',
                         day:
-                            'numeric' }) }}</time>
+                            'numeric'
+                    }) }}</time>
                     <span class="hidden lg:block text-muted">&middot;</span>
                     <!-- tags -->
                     <div>
-                        <span
-                            v-for="(tag, idx) in lesson.tags"
-                            :key="tag"
-                            class="text-warning-600 font-light"
-                        >
+                        <span v-for="(tag, idx) in lesson.tags" :key="tag" class="text-warning-600 font-light">
                             {{ tag }}<span v-if="idx !== lesson.tags.length - 1">, </span>
                         </span>
                     </div>
@@ -162,46 +139,25 @@ const progressButtonProps = computed<{
             </template>
 
             <div class="flex flex-col md:flex-row md:items-center gap-2 mt-4">
-                <h3
-                    v-if="lesson.biblical_references?.length"
-                    class="text-sm font-medium text-muted"
-                >
+                <h3 v-if="lesson.biblical_references?.length" class="text-sm font-medium text-muted">
                     Références bibliques :
                 </h3>
                 <div class="flex flex-wrap items-center gap-2">
                     <!-- biblical reference -->
-                    <UButton
-                        v-for="ref in lesson.biblical_references"
-                        :key="ref"
-                        :label="ref"
-                        color="secondary"
-                        variant="subtle"
-                        size="xs"
-                        :to="parseToVerse(ref)"
-                        target="_blank"
-                    />
+                    <UButton v-for="ref in lesson.biblical_references" :key="ref" :label="ref" color="secondary"
+                        variant="subtle" size="xs" :to="parseToVerse(ref)" target="_blank" />
                 </div>
             </div>
         </UPageHeader>
 
         <UPage>
             <UPageBody>
-                <ContentRenderer
-                    v-if="lesson"
-                    :value="lesson"
-                />
+                <ContentRenderer v-if="lesson" :value="lesson" />
 
                 <USeparator v-if="surround?.length" />
 
                 <UContentSurround :surround="surround" />
             </UPageBody>
-
-            <template
-                v-if="lesson?.body?.toc?.links?.length"
-                #right
-            >
-                <UContentToc :links="lesson.body.toc.links" />
-            </template>
         </UPage>
     </UContainer>
 </template>
