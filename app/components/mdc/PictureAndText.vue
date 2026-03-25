1b0ea467897cbe6a928688cc4e8b6a024360732e
Micky-N <ndingamickael@gmail.com>
2026-03-26 00:21:08 +0100

    refactor: simplifier le composant PictureAndText et ajuster le rendu du composant Pictures

--- app/components/mdc/PictureAndText.vue
+++ app/components/mdc/PictureAndText.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { UPageCard } from '#components';
+import { UCard } from '#components';
 
 defineProps<{
     reverse?: boolean;
@@ -8,43 +8,14 @@ defineProps<{
 </script>
 
 <template>
-    <component
-        :is="card ? UPageCard : 'div'"
-        class="my-8"
-        orientation="horizontal"
-        :reverse="reverse"
-    >
-        <template
-            v-if="card"
-            #default
-        >
-            <slot
-                mdc-unwrap="p"
-                name="image"
-            />
-        </template>
-        <div
-            v-if="!card"
-            class="grid md:grid-cols-3 items-center w-full gap-8"
-        >
-            <div
-                class="w-full"
-                :class="{ 'order-2': reverse }"
-            >
-                <slot
-                    mdc-unwrap="p"
-                    name="image"
-                />
+    <component :is="card ? UCard : 'div'">
+        <div class="grid grid-cols-3 items-center w-full gap-x-8">
+            <div :class="{ 'order-2': reverse }">
+                <slot name="image" />
             </div>
-            <div class="md:col-span-2">
-                <slot mdc-unwrap="p" />
+            <div class="col-span-2">
+                <slot />
             </div>
         </div>
-        <template
-            v-if="card"
-            #body
-        >
-            <slot />
-        </template>
     </component>
 </template>
